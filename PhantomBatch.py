ef75f98217813fbc5810391257859190f6c96a73
Josh <josh.calcino@gmail.com>
2018-08-17 15:00:42 +1000

    debug

--- PhantomBatch.py
+++ PhantomBatch.py
@@ -29,6 +29,8 @@ def dir_func(dirs, string, dict_arr):
     for i in range(0, len(dict_arr)):
         tmp_dir[i] = string+str(dict_arr[i]).replace('.', '')
 
+    print(tmp_dir)
+
     print(dirs)
 
     dirs = [dirs[i] + tmp_dir[j] for i in range(0, len(dirs)) for j in range(0, len(tmp_dir))]
