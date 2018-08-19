5793279e1b809047c1a8d493cb8e5f1afaf41c70
Josh <josh.calcino@gmail.com>
2018-08-20 08:56:36 +1000

    debug

--- PhantomBatch.py
+++ PhantomBatch.py
@@ -132,7 +132,7 @@ def initiliase_phantom(pbconf):
 
 def setup_from_array(setup_strings, string, dict_arr):
     print(len(setup_strings))
-    if len(setup_strings) is 0:
+    if len(setup_strings) is 1:
         setup_strings = [string + ' = ' + str(i) for i in dict_arr]
         print(setup_strings)
         return setup_strings
