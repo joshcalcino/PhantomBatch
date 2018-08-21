14472830db79bb97541adaeebb2f252dd25c30b5
Josh <josh.calcino@gmail.com>
2018-08-21 15:11:18 +1000

    Debug

--- PhantomBatch.py
+++ PhantomBatch.py
@@ -423,7 +423,8 @@ def run_batch_jobs(pbconf):
             print(job not in cjob)
             print('job_limit' in pbconf and len(current_jobs) < pbconf['job_limit'])
             if job not in cjob and ('job_limit' in pbconf and len(current_jobs) < pbconf['job_limit']):
-                submit_job(pbconf, pbconf['sim_dirs'][i], pbconf['setup'] + '.jobscript')
+                print('Would have tried to submit job '+pbconf['sim_dirs'][i])
+                # submit_job(pbconf, pbconf['sim_dirs'][i], pbconf['setup'] + '.jobscript')
         i += 1
 
 
