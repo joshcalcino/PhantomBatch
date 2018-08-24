879e56c3c8f5bc8c68483e7f628abae4b6d811fb
Josh <josh.calcino@gmail.com>
2018-08-24 12:37:06 +1000

    Debug

--- PhantomBatch.py
+++ PhantomBatch.py
@@ -160,7 +160,7 @@ def setup_from_array(setup_strings, string, dict_arr):
     for i in range(0, len(dict_arr)):
         tmp_setup_strings[i] = string + ' = ' + str(dict_arr[i])
     print(len(tmp_setup_strings))
-    setup_strings = [setup_strings[i] + [tmp_setup_strings[j]] for i in range(0, len(setup_strings))
+    setup_strings = [setup_strings[i] + tmp_setup_strings[j] for i in range(0, len(setup_strings))
                      for j in range(0, len(tmp_setup_strings))]
 
     # if isinstance(setup_strings[0], list):
