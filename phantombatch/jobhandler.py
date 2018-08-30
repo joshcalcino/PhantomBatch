ab603f0b95f24485c304e84536a41ad1fe627dd5
Josh <josh.calcino@gmail.com>
2018-08-30 14:45:44 +1000

    Fix verbose

--- phantombatch/jobhandler.py
+++ phantombatch/jobhandler.py
@@ -94,6 +94,7 @@ def decipher_slurm_output(slurm_output, pbconf):
 def decipher_pbs_output(pbs_output, pbconf):
     """ This function deciphers the output from pbs in the terminal """
     print(pbs_output)
+    pbs_output = pbs_output.rstrip()
     tally = 0
     tally_arr = []
     found_dash = False
