dd58bff5e6fc0896c3a20818cfa404bde61b7c18
Josh <josh.calcino@gmail.com>
2018-08-30 16:12:09 +1000

    Fix PBS output

--- phantombatch/jobhandler.py
+++ phantombatch/jobhandler.py
@@ -114,7 +114,8 @@ def decipher_pbs_output(pbs_output, pbconf):
         #     tally = 0
         elif not char.isalpha() and char != '-' and not char.isspace():
             # tally += 1
-            # tally_arr.append(tally)
+            if tally != 0:
+                tally_arr.append(tally)
             break
 
     print(tally)
