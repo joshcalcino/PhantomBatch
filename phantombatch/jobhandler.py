1c27dd21c8bbbbc717867a22e4a81cd150ee6aa6
Josh <josh.calcino@gmail.com>
2018-08-31 13:06:20 +1000

    Fix PBS output

--- phantombatch/jobhandler.py
+++ phantombatch/jobhandler.py
@@ -219,11 +219,11 @@ def check_running_jobs(pbconf):
 
     for line in my_jobs:
         if any([job in line[1] for job in pbconf['job_names']]):  # line[1] holds the name of the job in my_job
-            print('Adding in a new line to my_pb_jobs..')
+            log.debug('Adding in a new line to my_pb_jobs..')
             my_pb_jobs.append(line)
 
-    # log.debug('Printing phantombatch jobs..')
-    # log.debug(my_pb_jobs)
+    log.debug('Printing phantombatch jobs..')
+    log.debug(my_pb_jobs)
     return my_pb_jobs
 
 
