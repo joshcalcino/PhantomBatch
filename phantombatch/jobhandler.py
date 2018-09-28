46d8f7f7160a6f9d1e14e8fb648eafbf98b11f3d
Josh <josh.calcino@gmail.com>
2018-09-28 10:10:57 +1000

    Debug

--- phantombatch/jobhandler.py
+++ phantombatch/jobhandler.py
@@ -145,7 +145,9 @@ def check_running_jobs(pbconf):
     else:
         log.error('Job scheduler not recognised!')
 
+    log.debug('Filtering through the job scheduler output to find PhantomBatch jobs..')
     for line in my_jobs:
+        log.debug(line)
         if any([job in line[1] for job in pbconf['job_names']]):  # line[1] holds the name of the job in my_job
             log.debug('Adding in a new line to my_pb_jobs..')
             my_pb_jobs.append(line)
