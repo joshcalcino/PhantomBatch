d24c3e4e8c718a90fd5c638c08b43df2265f59f4
Josh <josh.calcino@gmail.com>
2018-10-28 15:05:15 +1000

    debug

--- phantombatch/splashhandler.py
+++ phantombatch/splashhandler.py
@@ -173,6 +173,8 @@ def move_images(pbconf):
             for file in files:
                 split_file_path = file.replace('simulations', 'splash').split(os.sep)
                 print(split_file_path)
+                print(split_file_path[:-1])
+                print(split_file_path[-1])
                 file_path = os.path.join(split_file_path[:-1], 'images', split_file_path[-1])
                 shutil.move(file, file_path)
 
